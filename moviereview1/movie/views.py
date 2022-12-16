6d2c1e7f2cca9827cfe0fe7e1aecbefb78348b63
Wanda <wboguslawska03@gmail.com>
2022-12-16 16:01:19 +0100

    Dodanie funkcji dodania aktora

--- moviereview1/movie/views.py
+++ moviereview1/movie/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
+from .models import Actor
 
 def index(request):
-    template = loader.get_template('page.html')
-    return HttpResponse(template.render())
+    actor = Actor.objects.all().values()
+    template = loader.get_template('index.html')
+    context = {
+        'actors': actor,
+    }
+    return HttpResponse(template.render(context, request))
+
+def add(request):
+  template = loader.get_template('add.html')
+  return HttpResponse(template.render({}, request))
+
+def addrecord(request):
+  name = request.POST['name']
+  surname = request.POST['surname']
+  member = Actor(Name=name, Surname=surname)
+  member.save()
+  return HttpResponseRedirect(reverse('index'))
