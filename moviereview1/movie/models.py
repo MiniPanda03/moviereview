78b9850e06d4680e45321ecd5ee05d5647a77c09
Wanda <wboguslawska03@gmail.com>
2022-12-16 11:26:18 +0100

    Panel administratora i modele

--- moviereview1/movie/models.py
+++ moviereview1/movie/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Actor (models.Model):
+    Name = models.CharField(max_length=255)
+    Surname = models.CharField(max_length=255)
+
+
+class Director (models.Model):
+    Name = models.CharField(max_length=255)
+    Surname = models.CharField(max_length=255)
+
+class Movie (models.Model):
+    Name =  models.CharField(max_length=255)
+    Description = models.CharField(max_length=500)
+    Slug = models.SlugField(max_length=40)
+    Director_id = models.ForeignKey(Director)
+    Avg_raiting = models.DecimalField(max_digits=5,decimal_places=2)
+    created = models.DateTimeField()
+    updated = models.DateTimeField()
+    raitingAmount = models.IntegerField()
+
+
+
+class Actors (models.Model):
+    Movie_id = models.ForeignKey(Movie)
+    Actor_id = models.ForeignKey(Actor)
+
+class Raiting(models.Model):
+    raitingValue = models.IntegerField()
+    user_id = models.IntegerField()
+    movie_id = models.ForeignKey(Movie)
