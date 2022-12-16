066df0d10d70baf79f5dd52349c8d0024d7d43ae
Wanda <wboguslawska03@gmail.com>
2022-12-16 15:07:29 +0100

    Dodanie strony w movies, zmiany w models

--- moviereview1/movie/models.py
+++ moviereview1/movie/models.py
@@ -13,7 +13,7 @@ class Movie (models.Model):
     Name =  models.CharField(max_length=255)
     Description = models.CharField(max_length=500)
     Slug = models.SlugField(max_length=40)
-    Director_id = models.ForeignKey(Director)
+    Director_id = models.ForeignKey(Director,null=True,on_delete=models.SET_NULL)
     Avg_raiting = models.DecimalField(max_digits=5,decimal_places=2)
     created = models.DateTimeField()
     updated = models.DateTimeField()
@@ -22,10 +22,10 @@ class Movie (models.Model):
 
 
 class Actors (models.Model):
-    Movie_id = models.ForeignKey(Movie)
-    Actor_id = models.ForeignKey(Actor)
+    Movie_id = models.ForeignKey(Movie,on_delete=models.CASCADE)
+    Actor_id = models.ForeignKey(Actor,on_delete=models.CASCADE)
 
 class Raiting(models.Model):
     raitingValue = models.IntegerField()
     user_id = models.IntegerField()
-    movie_id = models.ForeignKey(Movie)
+    movie_id = models.ForeignKey(Movie,on_delete=models.CASCADE)
